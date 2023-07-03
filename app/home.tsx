6a2a5ad59fb9952c0de9aeaac5c3192d21582c2c
Srini L <sjlaliwala246@gmail.com>
2023-07-03 01:11:55 -0400

    finish basic mvp (no cards for playtesters)

--- app/home.tsx
+++ app/home.tsx
@@ -1,13 +1,11 @@
 "use client";
-import { useRef } from "react";
-
 import { useState } from "react";
 
 const PLAYTESTERS = "For Playtesters";
 const COMPANIES = "For Companies";
 
 const Card = ({ title, description, children }: any) => (
-  <div className="flex flex-col shadow p-5 rounded text-left">
+  <div className="flex flex-col shadow p-5 rounded text-left outline-2 outline-lime-400">
     <h3 className="flex justify-center font-semibold md:text-lg text-base pb-1">
       {title}
     </h3>
@@ -21,86 +19,111 @@ const UNSELECTED_BUTTON_STYLE = (rounded: string) =>
 const SELECTED_BUTTON_STYLE = (rounded: string) =>
   `${BASE_BUTTON_STYLE} ${rounded} bg-lime-400 hover:bg-lime-300 active:bg-lime-400`;
 
-function HowItWorks() {
-  const [toggle, setToggle] = useState(COMPANIES);
+
+const PlaytestersHome = () => {
   return (
-    <>
-      <div className="flex flex-row justify-center">
-        <button
-          className={
-            toggle === COMPANIES
-              ? SELECTED_BUTTON_STYLE("rounded-l")
-              : UNSELECTED_BUTTON_STYLE("rounded-l")
-          }
-          onClick={() => setToggle(COMPANIES)}
-        >
-          {COMPANIES}
-        </button>
-        <button
-          className={
-            toggle === PLAYTESTERS
-              ? SELECTED_BUTTON_STYLE("rounded-r")
-              : UNSELECTED_BUTTON_STYLE("rounded-r")
-          }
-          onClick={() => setToggle(PLAYTESTERS)}
-        >
-          {PLAYTESTERS}
-        </button>
+    <div className="flex flex-col mt-6 items-center justify-center">
+      <div className="pb-10 border-b-2 border-lime-400">
+        <h1 className="font-semibold md:text-3xl text-2xl">
+          Test out the newest
+          <p>
+            <span className="text-lime-500">
+              sports equipment
+            </span>
+          </p>
+        </h1>
+        <div className="flex flex-col mt-6 items-center">
+          <label className="font-semibold">
+            Invite only. Have a referral?
+          </label>
+          <a
+            href="https://forms.gle/Ve5CEw3DVN68AfCo9"
+            target="_blank"
+            className="mt-2 p-3 bg-lime-400 hover:bg-lime-300 active:bg-lime-400 rounded"
+          >
+            Sign up to be a playtester!
+          </a>
+        </div>
+        <div className="mt-6">
+          <Support />
+        </div>
       </div>
-      <div className="mx-10 mt-5 grid md:grid-cols-3 sm:grid-cols-1 gap-5">
-        {toggle === COMPANIES ? (
-          <>
-            <Card title="Plan">
-              Strategically plan out the requirements of your playtest
-              (location, timeline, consumer breakdown, additional questions,
-              etc…)
-            </Card>
-            <Card title="Run Playtest">
-              Select your playtesters, ship out the products (and return
-              labels), and view the playtest reviews in real time. Once the
-              playtest ends, we will ensure that your products are returned.
-            </Card>
-            <Card title="Analyze">
-              Filter & analyze quantitative (average ratings) & qualitative
-              measures (open ended questions) across one or all of your
-              playtests to uncover key insights. For custom analysis, export the
-              data to use in your own systems.
-            </Card>
-          </>
-        ) : (
-          <>
-            <Card title="Apply Instantly">
-              Once you are an eligible playtester for a sport, immediately apply
-              for any related playtest. If it is your first time applying to
-              playtest in a new sport, you will be verified & onboarded shortly
-              after, so you can start playtesting right away
-            </Card>
-            <Card title="Test">
-              Upon being selected for a playtest, the product and a return label
-              will be shipped, and you will receive a form to submit your
-              feedback by the deadline.
-            </Card>
-            <Card title="Earn climeibility">
-              Your honest feedback is valuable, so the more playtests you
-              complete successfully, the more likely you will be chosen in
-              future playtests.
-            </Card>
-          </>
-        )}
-      </div>
-    </>
-  );
+    </div>
+  )
 }
+
+const CompaniesHome = () => (
+  <div className="flex flex-col mt-6 items-center justify-center">
+    <div className="pb-10 border-b-2 border-lime-400">
+      <h1 className="font-semibold md:text-3xl text-2xl">
+        A product testing platform for
+        <p>
+          <span className="text-lime-500">
+            sports equipment brands
+          </span>
+        </p>
+      </h1>
+      <div className="flex flex-col mt-6 items-center">
+        <label className="font-semibold">
+          Testing out a new product?
+        </label>
+        <a
+          href="https://forms.gle/VbbEth7kiT2AoY56A"
+          target="_blank"
+          className="mt-2 p-3 bg-lime-400 hover:bg-lime-300 active:bg-lime-400 rounded"
+        >
+          Sign up to run a playtest!
+        </a>
+      </div>
+      <div className="mt-6">
+        <Support />
+      </div>
+      
+    </div>
+
+    <div className="mt-10 md:mx-24 mx-5 grid md:grid-cols-3 grid-cols-1 gap-5">
+      <Card title="Verified Playtesters ✅">
+        Every playtester is verified to ensure
+        that they consistently play the sport 
+        at the skill level they list 
+      </Card>
+      <Card title="No-Hassle Sign Up 📝">
+        Start the playtesting process today. After signing up,
+        we'll schedule a call to gather your requirements
+        and go to work.
+      </Card>
+      <Card title="Powerful, Real-Time Insights 🔎">
+        Analyze playtest reviews as they are submitted by skill level, 
+        player attributes, aggregate ratings, and more. You'll be able 
+        to export this data to use for further custom analysis. 
+      </Card>
+
+    </div>
+
+  </div>
+)
+
+const Support = () => (
+  <h3>
+    Questions?
+    <p className="font-semibold">support@playtest.fyi</p>
+  </h3>
+)
 
 export default function Home() {
   const [toggle, setToggle] = useState(COMPANIES);
-  const playtestsSignUpRef = useRef(null);
-  const scrollToPlaytestsSignUp = () =>
-    playtestsSignUpRef.current?.scrollIntoView({ behavior: "smooth" });
   return (
-    <div className="flex py-24 border-b-2 justify-center items-center">
+    <div className="flex pt-20 pb-10 justify-center items-center">
+
       <div className="flex flex-col text-center">
-        <div className="flex flex-row justify-center">
+        
+        <div className="md:mx-72 mx-5 font-semibold border-lime-400 border-2 p-3">
+          <h3 className="">Disclaimer: This is an alpha version. We know you'll love playtest.fyi as much as we do. 
+            In fact, if for any reason you're not completely satisfied, we'll issue a full refund 💸.
+          </h3>
+        </div>
+        
+        <div className="mt-2 flex flex-row justify-center">
           <button
             className={
               toggle === COMPANIES
@@ -123,74 +146,12 @@ export default function Home() {
           </button>
         </div>
 
-        <div className="flex flex-col mt-6 items-center justify-center">
-          {toggle === COMPANIES ? (
-            <>
-              <h1 className="font-semibold md:text-3xl text-2xl">
-                A product testing platform for
-                <p>
-                  <span className="text-lime-500">
-                    sports equipment brands
-                  </span>
-                </p>
-              </h1>
-
-              <div className="flex flex-col mt-6">
-                <label className="font-semibold">
-                  Testing out a new product?
-                </label>
-                <a
-                  href="https://forms.gle/VbbEth7kiT2AoY56A"
-                  target="_blank"
-                  className="mt-2 p-3 bg-lime-400 hover:bg-lime-300 active:bg-lime-400 rounded"
-                >
-                  Sign up to run a playtest!
-                </a>
-              </div>
-            </>
-          ) : (
-            <>
-              <h1 className="font-semibold md:text-3xl text-2xl">
-                Test out the newest
-                <p>
-                  <span className="text-lime-500">
-                    sports equipment
-                  </span>
-                </p>
-              </h1>
-              <div className="flex flex-col mt-6">
-                <label className="font-semibold">
-                  Interested in product testing?
-                </label>
-                <button
-                  className="mt-2 p-3 bg-lime-400 hover:bg-lime-300 active:bg-lime-400 rounded"
-                  onClick={scrollToPlaytestsSignUp}
-                >
-                  👇 Apply for a playtest below 👇
-                </button>
-              </div>
-            </>
-          )}
-        </div>
-        <h3 className="mt-7 pb-10 border-b-2">
-          Questions?
-          <p className="font-semibold">support@playtest.fyi</p>
-        </h3>
-
-        <div className="flex flex-col mt-10 items-center justify-center">
-          {toggle === COMPANIES ? (
-            <>
-              
-            </>
-          ) : (
-            <>
-              <h2  className="font-semibold md:text-3xl text-2xl">
-                Playtests
-              </h2>
-              <p ref={playtestsSignUpRef} className="mt-3">Coming soon...</p>
-            </>
-          )}
-        </div>
+        {toggle === COMPANIES ? (
+          <CompaniesHome />
+        ) : (
+          <PlaytestersHome />
+        )}
+        
       </div>
     </div>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -6,34 +6,27 @@ const Logo = () => (
   </header>
 )
 
-
-
-
 export default async function HomePage() {
 
   return (
-    <main className="flex min-h-screen flex-col bg-white text-black">
-    
-      <nav className='fixed w-screen flex items-center p-3 bg-lime-500 shadow'>
-        <Logo />
-        <div className="flex md:flex-grow flex-row justify-end space-x-10">
-          {/* <a href="" className="p-2 text-lime-500 border-lime-500 hover:text-lime-400 hover:border-lime-400 active:text-lime-500 active:border-lime-500 border-b-4 font-semibold">
-            Log In</a> */}
+    <main>
+      <div className="flex flex-col bg-white text-black">
+        <nav className='fixed w-screen flex items-center p-4 bg-lime-500 shadow'>
+          <Logo />
+          <div className="flex md:flex-grow flex-row justify-end space-x-10">
+            {/* <a href="" className="p-2 text-lime-500 border-lime-500 hover:text-lime-400 hover:border-lime-400 active:text-lime-500 active:border-lime-500 border-b-4 font-semibold">
+              Log In</a> */}
+          </div>
+        </nav>
+
+        <Home />
+        
+        <div className='flex justify-center p-3 bg-lime-500 border-b-2'>
+          <header className='text-base text-white font-semibold'>
+            <a href='https://www.playtest.fyi'>Playtest.fyi</a>
+          </header>
         </div>
-      </nav>
-
-      
-
-      <Home />
-
-
-
-      <div className='flex justify-center p-4 bg-lime-500 border-b-2'>
-        <header className='text-base text-white font-semibold'>
-          <a href='https://www.playtest.fyi'>Playtest.fyi</a>
-        </header>
       </div>
-    
     </main>
   )
 }
